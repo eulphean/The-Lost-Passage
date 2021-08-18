c0ae76fd0bd609dd09d3b9c42426fff8a66fb5ec
Amay Kataria <amaykataria@Amays-Air.attlocal.net>
2021-08-18 05:24:06 -0500

    Updates

---
+++ precache-manifest.878583e6b83c77255626bb8cb719bc4d.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ef38b3ca21efb0762d289c5b13018bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cfa1d5e4e49fe13a5c89",
+    "url": "/static/css/main.562eed68.chunk.css"
+  },
+  {
+    "revision": "73b76d4a2c66db1e4365",
+    "url": "/static/js/2.38d0c373.chunk.js"
+  },
+  {
+    "revision": "e1e3d6106dc79d4567113c24c9b993d8",
+    "url": "/static/js/2.38d0c373.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfa1d5e4e49fe13a5c89",
+    "url": "/static/js/main.c7539315.chunk.js"
+  },
+  {
+    "revision": "fe21d40666764c425af7",
+    "url": "/static/js/runtime-main.8b5d6628.js"
+  },
+  {
+    "revision": "e13ce42a9d64e753315088909c4bed53",
+    "url": "/static/media/demode-webfont.e13ce42a.woff"
+  },
+  {
+    "revision": "f4adb378be7be27778730bb821970f70",
+    "url": "/static/media/demode-webfont.f4adb378.woff2"
+  },
+  {
+    "revision": "6acdacdc31196f4007a5cc48e1340130",
+    "url": "/static/media/din-webfont.6acdacdc.woff2"
+  },
+  {
+    "revision": "e6a361ef75e47721d5e73bbb557d7635",
+    "url": "/static/media/din-webfont.e6a361ef.woff"
+  },
+  {
+    "revision": "5ff4e0544347d214449dfc2f1989c979",
+    "url": "/static/media/jellyman.5ff4e054.glb"
+  },
+  {
+    "revision": "87b33e2de3819ca5260319ef546738c9",
+    "url": "/static/media/opensans-light-webfont.87b33e2d.woff2"
+  },
+  {
+    "revision": "9ad03de81bbf6cd48ffc44cf2aa4d707",
+    "url": "/static/media/opensans-light-webfont.9ad03de8.woff"
+  },
+  {
+    "revision": "65a61f9230a85b44df43968f80a522e5",
+    "url": "/static/media/opensans-regular-webfont.65a61f92.woff"
+  },
+  {
+    "revision": "d407a58546593548175615d7621183ef",
+    "url": "/static/media/opensans-regular-webfont.d407a585.woff2"
+  },
+  {
+    "revision": "37e7188300a1b8c7ee0b3074db77d86f",
+    "url": "/static/media/slice-webfont.37e71883.woff2"
+  },
+  {
+    "revision": "60013d1c9c4b0368a971b7814f6b98f3",
+    "url": "/static/media/slice-webfont.60013d1c.woff"
+  }
+]);
